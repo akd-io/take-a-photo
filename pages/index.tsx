48e90fad91cb0db2977e3cfee2f4a83671926546
Anders Kjær Damgaard <akd@akd.io>
2023-07-18 14:46:19 +0200

    Add Tailwind prettier plugin

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Index: NextPage = () => {
           content="A demo of different photo capture methods on the web."
         />
       </Head>
-      <div className="container px-4 mx-auto flex-col ">
+      <div className="container mx-auto my-10 flex flex-col gap-1 px-4">
         <h1 className="text-lg">Take a Photo</h1>
         <p className="text-base">
           This is a demo of different photo capture methods on the web.
